736fa9d974c4076b538e3eea04d58da61a16c74e
Nima Sherpa <nimadorji01@gmail.com>
2023-07-12 10:24:09 -0400

    Jason file added

--- node-shell/ExpressBasics/app.js
+++ node-shell/ExpressBasics/app.js
@@ -2,8 +2,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send(`
+app.get("/", (req, res) => {  // req: from client to server // res: from server to client //// res.send: responding with 'send' built in function 
+  res.send(`                    
     <html>
         <head>
             <Title>Cars and Trucks</Title>
@@ -18,11 +18,19 @@ app.get("/cars", (req, res) => {
   res.send(`cars`);
 });
 
+
 app.get("/trucks", (req, res) => {
   res.send(`trucks`);
 });
 
-const PORT = 1337;
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+// const PORT = 1337;
+// app.listen(PORT, () => {
+//   console.log(`listening on port ${PORT}`);
+// });
+
+
+// different way of opening PORT
+app.listen(1337, function(req, res){
+  console.log("running");
 });
+
